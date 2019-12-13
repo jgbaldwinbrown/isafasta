3025e562f13b54e1d8d993886bfe3b7037c53910
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2019-12-13 10:47:06 -0700

    made automated tests, moved test files to tests folder

---
+++ test.sh
@@ -0,0 +1,11 @@
+if ! ./isabed <tests/test1.bed >/dev/null 2>&1 ; then echo "test1.bed failed" ; fi
+if ./isabed <tests/test2.bed >/dev/null 2>&1 ; then echo "test2.bed failed" ; fi
+if ./isabed <tests/test3.bed >/dev/null 2>&1 ; then echo "test3.bed failed" ; fi
+if ./isafasta <tests/test1.fa >/dev/null 2>&1 ; then echo "test1.fa failed" ; fi
+if ./isafasta <tests/test2.fa >/dev/null 2>&1 ; then echo "test2.fa failed" ; fi
+if ./isafasta <tests/test3.fa >/dev/null 2>&1 ; then echo "test3.fa failed" ; fi
+if ! ./isafasta <tests/test4.fa >/dev/null 2>&1 ; then echo "test4.fa failed" ; fi
+if ./isafastq <tests/test1.fq >/dev/null 2>&1 ; then echo "test1.fq failed" ; fi
+if ./isafastq <tests/test2.fq >/dev/null 2>&1 ; then echo "test2.fq failed" ; fi
+if ./isafastq <tests/test3.fq >/dev/null 2>&1 ; then echo "test3.fq failed" ; fi
+if ! ./isafastq <tests/test4.fq >/dev/null 2>&1 ; then echo "test4.fq failed" ; fi
